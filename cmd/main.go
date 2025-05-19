465ba90cf70f9b7361be467a6ea35c41709a11a4
fabianusjericho <fabianusjeriko@gmail.com>
2025-05-19 20:52:06 +0700

    feat: add git ignore and configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/FJericho/url-shortener/config"
+)
+
 func main() {
-	
+	viper := config.NewViperConfig()
+	app := config.NewFiberConfig(viper)
+	log := config.NewLoggerConfig()
+	validate := config.NewValidatorConfig(viper)
+	db := config.NewDatabaseConfig(viper, log)
+
+	config.StartServer(&config.AppConfig{
+		App:      app,
+		DB:       db,
+		Validate: validate,
+		Viper:    viper,
+		Log:      log,
+	})
+
+	appPort := viper.GetInt("APP_PORT")
+
+	err := app.Listen(fmt.Sprintf(":%d", appPort))
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
